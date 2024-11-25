1d36494da046c568b001e405b3ff502f9bcf841b
TrueMajner <49881015+TrueMajner@users.noreply.github.com>
2024-11-25 21:38:45 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,30 @@
+#!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y docker.io docker-compose
-sudo apt install cron
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo systemctl start cron
-sudo systemctl enable cron
+
+if ! command -v docker &> /dev/null; then
+    sudo apt install -y docker.io
+    sudo systemctl enable docker
+    sudo systemctl start docker
+else
+    echo "Docker is already installed."
+fi
+
+if ! command -v docker-compose &> /dev/null; then
+    sudo apt install -y docker-compose
+else
+    echo "Docker Compose is already installed."
+fi
+
+if ! dpkg -l | grep -qw cron; then
+    sudo apt install -y cron
+    sudo systemctl enable cron
+    sudo systemctl start cron
+else
+    echo "Cron is already installed."
+fi
+
 docker-compose up -d
+
 chmod +x backup.sh
-crontab -e
-0 3 * * * /bin/bash /mongodb-docker-compose/backup.sh #change path
+
+(crontab -l 2>/dev/null; echo "0 3 * * * /bin/bash /mongodb-docker-compose/backup.sh") | crontab -
